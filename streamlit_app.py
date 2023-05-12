5151af38948f0d1713da1c6c9152b870539ea03f
mnsnowflake <133148497+mnsnowflake@users.noreply.github.com>
2023-05-12 16:16:40 -0400

    API Data load into pandas dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,4 +25,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+# Load the JSON to pandas dataframe
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Display the dataframe in streamlit
+streamlit.dataframe(fruityvice_normalized)
