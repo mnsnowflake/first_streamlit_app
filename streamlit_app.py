38179a1eaa95a4cf4fdfb98ac49e2eee270e06f6
mnsnowflake <133148497+mnsnowflake@users.noreply.github.com>
2023-05-19 13:16:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,11 +7,11 @@ import requests
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
-select * from pc_rivery_db.public.fruit_load_list
+ 
 
 
 streamlit.title("My Parents new Healthy Diner!")
